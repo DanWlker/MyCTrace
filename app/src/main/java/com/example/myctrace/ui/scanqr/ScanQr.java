1c06bc58c240f4f18e8a16f0a91036d61a8b0be9
DanWlker <danielhee2@gmail.com>
2021-11-24 05:09:53 +0800

    Done first draft of the pop up want die le
    arsdearosehtoiewantedahw fyonTEARd

--- app/src/main/java/com/example/myctrace/ui/scanqr/ScanQr.java
+++ app/src/main/java/com/example/myctrace/ui/scanqr/ScanQr.java
@@ -1,22 +1,38 @@
 package com.example.myctrace.ui.scanqr;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.myctrace.MainActivity;
 import com.example.myctrace.R;
 import com.example.myctrace.ui.EmulateQRScanningPage;
+import com.example.myctrace.ui.components.ComponentClickableUserPhoto;
 import com.example.myctrace.ui.login.Login;
 import com.example.myctrace.ui.register.Register;
 
@@ -49,7 +65,57 @@ public class ScanQr extends Fragment {
                 startActivity(intent);
             }
         });
-        // TODO: Use the ViewModel
+
+        view.findViewById(R.id.clickable_user_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.fragment_components_pop_up_add_user, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+
+                View container;
+
+                if (popupWindow.getBackground() == null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        container = (View) popupWindow.getContentView().getParent();
+                    } else {
+                        container = popupWindow.getContentView();
+                    }
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        container = (View) popupWindow.getContentView().getParent().getParent();
+                    } else {
+                        container = (View) popupWindow.getContentView().getParent();
+                    }
+                }
+
+                WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+                WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+                p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                p.dimAmount = 0.3f;
+                wm.updateViewLayout(container, p);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+            }
+        });
+
     }
 
 }
