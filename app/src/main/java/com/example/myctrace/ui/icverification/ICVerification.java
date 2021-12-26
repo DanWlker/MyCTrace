c850fbdb844818e06bbd9f359fb6c51de02d7b73
DanWlker <danielhee2@gmail.com>
2021-12-27 00:46:20 +0800

    Created qr scanning page

--- app/src/main/java/com/example/myctrace/ui/icverification/ICVerification.java
+++ app/src/main/java/com/example/myctrace/ui/icverification/ICVerification.java
@@ -19,18 +19,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowInsets;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.example.myctrace.MainActivity;
 import com.example.myctrace.databinding.ActivityIcverificationBinding;
 import com.example.myctrace.R;
 import com.example.myctrace.ui.login.Login;
-import com.example.myctrace.utilities.TempUserLogin;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
