900bc76117ac16c8f39df2b85d1a9b1edcdee4c0
DanWlker <danielhee2@gmail.com>
2021-12-27 18:48:26 +0800

    [WIP] Date picker construction done, need to link wifh firebase

--- app/src/main/java/com/example/myctrace/ui/checkinhistory/CheckInHistoryFragment.java
+++ app/src/main/java/com/example/myctrace/ui/checkinhistory/CheckInHistoryFragment.java
@@ -2,6 +2,7 @@ package com.example.myctrace.ui.checkinhistory;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.myctrace.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class CheckInHistoryFragment extends Fragment {
 
     private CheckInHistoryViewModel mViewModel;
+
+    EditText edTxtDate;
+    Calendar myCalendar = Calendar.getInstance();
 
     public static CheckInHistoryFragment newInstance() {
         return new CheckInHistoryFragment();
@@ -33,6 +44,39 @@ public class CheckInHistoryFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CheckInHistoryViewModel.class);
         // TODO: Use the ViewModel
+        View view = getView();
+
+        edTxtDate = view.findViewById(R.id.editTextDate);
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH, i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+                updateDisplay();
+            }
+        };
+
+        edTxtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(
+                        getActivity(),
+                        date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
+    }
+
+    private void updateDisplay() {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        format.setTimeZone(TimeZone.getTimeZone("Asia/Kuala_Lumpur"));
+        edTxtDate.setText(format.format(myCalendar.getTime()));
     }
 
 }
