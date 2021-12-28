53ab6686fffe74d6f522f07bcf747c96155023cf
DanWlker <danielhee2@gmail.com>
2021-12-28 13:06:03 +0800

    Completed show fully vaccination and fixed bug

--- app/src/main/java/com/example/myctrace/ui/checkin/CheckIn.java
+++ app/src/main/java/com/example/myctrace/ui/checkin/CheckIn.java
@@ -47,6 +47,7 @@ import com.example.myctrace.uireusablecomponents.checkInLocation.CheckInLocation
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -159,7 +160,68 @@ public class CheckIn extends Fragment {
 
     private void checkVacStatus(View v) {
         TextView txtViewVaccination = v.findViewById(R.id.txtViewVaccination);
-        TextView txtViewVaccinationDate = v.findViewById(R.id.txtViewRiskDate);
+        TextView txtViewVaccinationDate = v.findViewById(R.id.txtViewVaccinationDate);
+        LinearProgressIndicator progressBarVaccination = v.findViewById(R.id.progressBarVaccination);
+
+        mbase.child("vacInfo").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(!task.isSuccessful())
+                    return;
+
+                String dosage = "No Vaccination";
+                String dateTime = "0";
+
+                if(task.getResult().child("secondDose").exists()) {
+                    dosage = "Fully Vaccinated";
+                    dateTime = String.valueOf(task.getResult().child("secondDose").getValue());
+                } else if(task.getResult().child("firstDose").exists()) {
+                    dosage = "First Dose";
+                    dateTime = String.valueOf(task.getResult().child("firstDose").getValue());
+                }
+
+                Long dateTimeLong = Long.valueOf(dateTime);
+                Date date = new Date(dateTimeLong*1000);
+                DateFormat format = new SimpleDateFormat("dd MMMM yyyy");
+                format.setTimeZone(TimeZone.getTimeZone("Asia/Kuala_Lumpur"));
+                String formatted = format.format(date);
+
+                int trackColor;
+                int indicatorColor;
+                int progress;
+
+                switch (dosage) {
+                    case "Fully Vaccinated":
+                        indicatorColor = ContextCompat.getColor(v.getContext(), R.color.green_primary);
+                        trackColor = ContextCompat.getColor(v.getContext(), R.color.green_secondary);
+                        progress = 100;
+                        break;
+                    case "First Dose":
+                        indicatorColor = ContextCompat.getColor(v.getContext(), R.color.orange_primary);
+                        trackColor = ContextCompat.getColor(v.getContext(), R.color.orange_secondary);
+                        progress = 50;
+                        break;
+                    case "No Vaccination":
+                        indicatorColor = ContextCompat.getColor(v.getContext(), R.color.orange_primary);
+                        trackColor = ContextCompat.getColor(v.getContext(), R.color.orange_secondary);
+                        progress = 0;
+                        break;
+                    default:
+                        indicatorColor = ContextCompat.getColor(v.getContext(), R.color.blue_primary);
+                        trackColor = ContextCompat.getColor(v.getContext(), R.color.blue_secondary);
+                        progress = 0;
+                }
+
+                txtViewVaccination.setText(dosage);
+                txtViewVaccination.setTextColor(indicatorColor);
+                txtViewVaccinationDate.setText("Vaccination Date: " + formatted);
+                txtViewVaccinationDate.setTextColor(indicatorColor);
+                progressBarVaccination.setProgressCompat(progress, true);
+                progressBarVaccination.setIndicatorColor(indicatorColor);
+                progressBarVaccination.setTrackColor(trackColor);
+
+            }
+        });
     }
 
     private void checkRisk(View v) {
@@ -169,51 +231,51 @@ public class CheckIn extends Fragment {
         mbase.child("riskInfo").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()) {
+                if(!task.isSuccessful())
+                    return;
 
-                    String riskStatus = String.valueOf(task.getResult().child("riskStatus").getValue());
-                    String dateTime = String.valueOf(task.getResult().child("dateTime").getValue());
+                String riskStatus = String.valueOf(task.getResult().child("riskStatus").getValue());
+                String dateTime = String.valueOf(task.getResult().child("dateTime").getValue());
 
-                    txtViewRisk.setText(riskStatus);
 
-                    Long dateTimeLong = Long.valueOf(dateTime);
-                    Date date = new Date(dateTimeLong*1000);
-                    DateFormat format = new SimpleDateFormat("dd MMMM yyyy");
-                    format.setTimeZone(TimeZone.getTimeZone("Asia/Kuala_Lumpur"));
-                    String formatted = format.format(date);
+                Long dateTimeLong = Long.valueOf(dateTime);
+                Date date = new Date(dateTimeLong*1000);
+                DateFormat format = new SimpleDateFormat("dd MMMM yyyy");
+                format.setTimeZone(TimeZone.getTimeZone("Asia/Kuala_Lumpur"));
+                String formatted = format.format(date);
 
-                    txtViewRiskDate.setText(
-                            "Health Risk Assesment: " + formatted
-                    );
 
-                    int colorToSet;
+                int colorToSet;
 
-                    switch (riskStatus) {
-                        case "High":
-                            colorToSet = ContextCompat.getColor(getContext(), R.color.orange_primary);
-                            break;
-                        case "Low":
-                            colorToSet = ContextCompat.getColor(getContext(), R.color.green_primary);
-                            break;
-                        default:
-                            colorToSet = ContextCompat.getColor(getContext(), R.color.blue_primary);
-                    }
-
-                    txtViewRisk.setTextColor(colorToSet);
-                    txtViewRiskDate.setTextColor(colorToSet);
-
+                switch (riskStatus) {
+                    case "High":
+                        colorToSet = ContextCompat.getColor(v.getContext(), R.color.orange_primary);
+                        break;
+                    case "Low":
+                        colorToSet = ContextCompat.getColor(v.getContext(), R.color.green_primary);
+                        break;
+                    default:
+                        colorToSet = ContextCompat.getColor(v.getContext(), R.color.blue_primary);
                 }
+
+                txtViewRisk.setText(riskStatus);
+                txtViewRiskDate.setText(
+                        "Health Risk Assesment: " + formatted
+                );
+                txtViewRisk.setTextColor(colorToSet);
+                txtViewRiskDate.setTextColor(colorToSet);
+
             }
         });
 
     }
 
-    private void loadRecentCheckIns(View view) {
+    private void loadRecentCheckIns(View v) {
         //Firebase stuff
-        recyclerView = view.findViewById(R.id.checkInLocationRecycler);
+        recyclerView = v.findViewById(R.id.checkInLocationRecycler);
 
 
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(v.getContext());
         mLayoutManager.setReverseLayout(true);
         mLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(mLayoutManager);
